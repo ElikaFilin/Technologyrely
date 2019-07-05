0f8d6885e32c74c5c08c826feba31787158a2223
den_b_it@i.ua <1058ark7>
2019-07-05 14:21:44 +0300

    create debounce and throttle function

--- Lecture_5/debounce.js
+++ Lecture_5/debounce.js
@@ -3,5 +3,18 @@
  * time - время в миллисекундах
  */
 function debounce(callback, time) {
-  // Code
+	return function (args) {
+		let previousCall = this.lastCall;
+		this.lastCall = Date.now();
+		if (previousCall && ((this.lastCall - previousCall) <= time)) {
+			clearTimeout(this.lastCallTimer);
+		}
+		this.lastCallTimer = setTimeout(() => callback(args), time);
+	}
 }
+let log = (args) => console.log(`My args are ${args}`);
+let debouncedLog = debounce(log, 2000);
+debouncedLog([1, 2, 3]);
+debouncedLog([4, 5, 6]);
+debouncedLog([7, 8, 9]);
+
--- Lecture_5/throttle.js
+++ Lecture_5/throttle.js
@@ -3,5 +3,19 @@
  * time - время в миллисекундах
  */
 function throttle(callback, time) {
-  // Code
+	return function (args) {
+		let previousCall = this.lastCall;
+		this.lastCall = Date.now();
+		if (previousCall === undefined
+		(this.lastCall - previousCall) > time) {
+			callback(args);
+		}
+	}
 }
+let logger = (args) => console.log(`My args are ${args}`);
+let throttledLogger = throttle(logger, 2000);
+throttledLogger([1, 2, 3]);
+throttledLogger([1, 2, 3]);
+throttledLogger([1, 2, 3]);
+throttledLogger([1, 2, 3]);
+throttledLogger([1, 2, 3]);
