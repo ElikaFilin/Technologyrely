8b1778dbae092e03ab7a2c8198e1e4e3b72142a0
den_b_it@i.ua <1058ark7>
2019-07-14 21:49:07 +0300

    lecture_8

--- Lecture_8/prettify.js
+++ Lecture_8/prettify.js
@@ -3,8 +3,35 @@
 //   text
 //   <div>Bam</div>
 // </body>
+function prettify(document) {
+	var body = document.getElementsByTagName("body")[0];
+	run_prettify(body);
+}
+
+function run_prettify(el) {
+	let countChild = el.childElementCount;
+	for (let i = 0; i < countChild; i++) {
+
+		if (el.children[i].nodeName.toUpperCase() == "DIV") {
+			if (el.children[i].childElementCount > 0) {
+				run_prettify(el.children[i]);
+			} else if (el.children[i].firstChild && el.children[i].firstChild.nodeType === 3) {
+				console.log(el.children[i].nodeName);
+				let elem = document.createElement('p');
+				elem.innerHTML = el.children[i].innerHTML;
+				el.children[i].innerHTML = '';
+				el.children[i].appendChild(elem);
+			}
+		} else if (el.children[i].nodeName.toUpperCase() == "P") {
+			continue
+		}
+
+
+	}
+}
+
 const elements = prettify(document);
-console.log(document.body.innerHTML);
+//console.log(document.body.innerHTML);
 // <body>
 //   <p>Boom</p>
 //   text
--- Lecture_8/recSearchTags.js
+++ Lecture_8/recSearchTags.js
@@ -3,6 +3,25 @@
 //   text
 //   <div><p>2</p></div>
 // </body>
-const elements = recSearchTags(document, 'p');
+
+function recSearchTags(document, tagName){
+		let tagList = [];
+		let htmlcode=window.document.documentElement.innerHTML;
+		let from=0, to, text;
+		while(true){
+			from=htmlcode.indexOf("<"+tagName,from);
+			if(from===-1){
+				break;
+			}
+			to=htmlcode.indexOf(">", from);
+			text=htmlcode.substring(from, to+1);
+			tagList.push(text);
+			from=to;
+		}
+	console.log(tagList);
+}
+
+recSearchTags(document, "p");
+//const elements = recSearchTags(document, 'p');
 // ['<p>1</p>' '<p>2</p>'] где каждый элемент это объект соответствующего типа
-console.log(elements.length); // 2
+//console.log(elements.length); // 2
